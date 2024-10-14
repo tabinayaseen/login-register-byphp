64480c8ad8ac9dd75710d8b89a63bf8237b29b04
Tabina yaseen <130529454+tabinayaseen@users.noreply.github.com>
2024-10-14 06:56:51 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+    header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                } else {
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            } else {
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+        <h2>Login to Your Account</h2>
+        <form action="login.php" method="post">
+            <div class="form-group">
+                <input type="email" placeholder="Enter Email" name="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Enter Password" name="password" class="form-control" required>
+            </div>
+            <div class="form-btn">
+                <input type="submit" value="Login" name="login" class="btn btn-primary">
+            </div>
+        </form>
+        <p>Not registered yet? <a href="registration.php">Register Here</a></p>
+        <div class="linkedin">
+            <p>Connect with me on <a href="https://www.linkedin.com/in/tabeena-yaseen-s131k00h6">LinkedIn</a></p>
+        </div>
+    </div>
+</body>
+</html>
